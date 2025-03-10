b20b0b9620eff506fa990331229c0e3675cf2ce1
Roy538 <mauriceroy538@gmail.com>
2025-03-10 18:01:35 +0300

    cal.js

--- tools/cal.js
+++ tools/cal.js
@@ -1,23 +1,23 @@
-function calculateSIP() {
-    const monthlyInvestment = parseFloat(document.getElementById('monthlyInvestment').value);
-    const annualInterestRate = parseFloat(document.getElementById('annualInterestRate').value);
-    const investmentPeriod = parseFloat(document.getElementById('investmentPeriod').value);
+// function calculateSIP() {
+//     const monthlyInvestment = parseFloat(document.getElementById('monthlyInvestment').value);
+//     const annualInterestRate = parseFloat(document.getElementById('annualInterestRate').value);
+//     const investmentPeriod = parseFloat(document.getElementById('investmentPeriod').value);
 
-    if (isNaN(monthlyInvestment) || isNaN(annualInterestRate) || isNaN(investmentPeriod) || monthlyInvestment <= 0 || annualInterestRate <= 0 || investmentPeriod <= 0) {
-        document.getElementById('sipResult').innerHTML = `<p>Please enter valid positive values for all fields.</p>`;
-        return;
-    }
+//     if (isNaN(monthlyInvestment) || isNaN(annualInterestRate) || isNaN(investmentPeriod) || monthlyInvestment <= 0 || annualInterestRate <= 0 || investmentPeriod <= 0) {
+//         document.getElementById('sipResult').innerHTML = `<p>Please enter valid positive values for all fields.</p>`;
+//         return;
+//     }
 
-    const monthlyInterestRate = (annualInterestRate / 100) / 12;
-    const totalMonths = investmentPeriod * 12;
+//     const monthlyInterestRate = (annualInterestRate / 100) / 12;
+//     const totalMonths = investmentPeriod * 12;
 
-    const futureValue = monthlyInvestment * (((Math.pow(1 + monthlyInterestRate, totalMonths) - 1) / monthlyInterestRate) + 1);
+//     const futureValue = monthlyInvestment * (((Math.pow(1 + monthlyInterestRate, totalMonths) - 1) / monthlyInterestRate) + 1);
 
-    const sipResultElement = document.getElementById('sipResult');
-    sipResultElement.innerHTML = `<h3>Potential Returns:</h3>
-                               <p>If you invest INR ${monthlyInvestment} every month at an annual interest rate of ${annualInterestRate}%,</p>
-                               <p>your potential returns after ${investmentPeriod} years could be approximately INR ${futureValue.toFixed(2)}.</p>`;
-}
+//     const sipResultElement = document.getElementById('sipResult');
+//     sipResultElement.innerHTML = `<h3>Potential Returns:</h3>
+//                                <p>If you invest INR ${monthlyInvestment} every month at an annual interest rate of ${annualInterestRate}%,</p>
+//                                <p>your potential returns after ${investmentPeriod} years could be approximately INR ${futureValue.toFixed(2)}.</p>`;
+// }
 
 // function calculatePPF() {
 //     const principalAmount = parseFloat(document.getElementById('principalAmount').value);

82cdea6b5e3f4ec8c79fec15e8cf9a7d0631ce44
Roy538 <mauriceroy538@gmail.com>
2025-03-16 00:36:09 +0300

    calc

--- tools/sipcalculatorJs.js
+++ tools/sipcalculatorJs.js
@@ -93,14 +93,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-//     const knowMoreBtns = document.querySelectorAll(".know-more-btn");
-//     knowMoreBtns.forEach(button => {
-//         button.addEventListener("click", function () {
-//             const knowMoreContent = this.nextElementSibling;
-//             knowMoreContent.classList.toggle("show");
-//         });
-//     });
-// });
+    const knowMoreBtns = document.querySelectorAll(".know-more-btn");
+    knowMoreBtns.forEach(button => {
+        button.addEventListener("click", function () {
+            const knowMoreContent = this.nextElementSibling;
+            knowMoreContent.classList.toggle("show");
+        });
+    });
+});
 
 const commas = () => {
     let amount = document.getElementById("investment").value;

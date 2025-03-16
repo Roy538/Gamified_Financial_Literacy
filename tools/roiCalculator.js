d1d507f7b3cd2440fbda5744457c24557010c524
Roy538 <mauriceroy538@gmail.com>
2025-03-16 18:59:44 +0300

    ROI

--- tools/roiCalculator.js
+++ tools/roiCalculator.js
@@ -23,8 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
 //     document.getElementById('result').textContent = `ROI: ${roi.toFixed(2)}%`;
 // }
 
-// function clearROI() {
-//     document.getElementById('roi-form').reset();
-//     document.getElementById('gl').innerText = '';
-//     document.getElementById('result').innerText = '';
-// }
+function clearROI() {
+    document.getElementById('roi-form').reset();
+    document.getElementById('gl').innerText = '';
+    document.getElementById('result').innerText = '';
+}

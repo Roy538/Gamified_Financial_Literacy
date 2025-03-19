9578872d85a43ecb7c72f2d32fb9ba1f6a2fd957
Roy538 <mauriceroy538@gmail.com>
2025-03-19 22:22:03 +0300

    GST

--- tools/GSTcalc.js
+++ tools/GSTcalc.js
@@ -58,15 +58,15 @@ function validateInputs(amount, taxSlab) {
     return hasError;
 }
 
-// function showError(elementId, message) {
-//     document.getElementById(elementId).textContent = message;
-// }
+function showError(elementId, message) {
+    document.getElementById(elementId).textContent = message;
+}
 
-// function clearErrorMessages() {
-//     document.getElementById('amountError').textContent = '';
-//     document.getElementById('taxSlabError').textContent = '';
-//     document.getElementById('modeError').textContent = '';
-// }
+function clearErrorMessages() {
+    document.getElementById('amountError').textContent = '';
+    document.getElementById('taxSlabError').textContent = '';
+    document.getElementById('modeError').textContent = '';
+}
 
 // function clearGST() {
 //     document.getElementById('gst-calculator').reset();

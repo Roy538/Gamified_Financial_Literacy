5a986d3b70a26ff2b178cbc4bdc428e7508ded93
Roy538 <mauriceroy538@gmail.com>
2025-03-25 22:20:45 +0300

    final

--- final.js
+++ final.js
@@ -36,11 +36,11 @@ document.getElementById("cancelButton").addEventListener("click", function () {
 });
 
 // Handle image upload
-// document
-//   .getElementById("imageUpload")
-//   .addEventListener("change", function (event) {
-//     const file = event.target.files[0];
-//     const reader = new FileReader();
+document
+  .getElementById("imageUpload")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
     reader.onload = function (e) {
       document.getElementById("profileImage").src = e.target.result; // Update the image src to the new file

4561de33ae604bc1de4674bc78c58202c106e898
Roy538 <mauriceroy538@gmail.com>
2025-04-02 20:16:35 +0300

    2-April

--- FinTech/popup.js
+++ FinTech/popup.js
@@ -17,15 +17,15 @@ window.onload = function() {
   // });
   
   // Handle form submission
-  document.getElementById('emailForm-nl').addEventListener('submit', function(event) {
-    event.preventDefault();
+  // document.getElementById('emailForm-nl').addEventListener('submit', function(event) {
+  //   event.preventDefault();
   
-    const email = document.getElementById('email-nl').value;
-    if (email) {
-      alert(`Your email ID ${email} has been registered successfully for the newsletter.`);
-      document.getElementById('popup').style.display = 'none';
-    }
-  });
+  //   const email = document.getElementById('email-nl').value;
+  //   if (email) {
+  //     alert(`Your email ID ${email} has been registered successfully for the newsletter.`);
+  //     document.getElementById('popup').style.display = 'none';
+  //   }
+  // });
   
   // Handle "No thanks" link
   document.querySelector('.no-thanks-nl').addEventListener('click', function(event) {

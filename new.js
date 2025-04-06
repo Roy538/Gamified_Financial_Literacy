2d6ab178b0c650ec015515aa5f8b0012fd265e8c
Roy538 <mauriceroy538@gmail.com>
2025-04-06 17:47:43 +0300

    5-April

--- new.js
+++ new.js
@@ -150,7 +150,7 @@ function createPaginationButtons() {
     // button.classList.remove("active");
     // if (parseInt(button.textContent) === currentPage) {
       // button.classList.add("active");
-    }
+    // }
   });
 // }
 

b8aa6c456146d794fd751e6fd668824c7e0f9179
Roy538 <mauriceroy538@gmail.com>
2025-05-06 21:17:35 +0300

    6-May

--- tools/roiCalculator.js
+++ tools/roiCalculator.js
@@ -21,7 +21,7 @@
 
     // document.getElementById('gl').textContent = `Gain: Ksh${gl.toFixed(2)}`;
     // document.getElementById('result').textContent = `ROI: ${roi.toFixed(2)}%`;
-}
+// }
 
 function clearROI() {
     document.getElementById('roi-form').reset();
